5f34aecfbb6aec452bf2f6e7c60a398d19c95343
Afifa Dar <afifadar2001@gmail.com>
2022-10-01 00:40:57 +0500

    Qr-code

---
+++ jsCalculator/accessible.js
@@ -0,0 +1,194 @@
+//define operators
+const operator = {
+    plus : (a , b) => a + b ,
+    sub : (a , b) => a - b ,
+    mul : (a , b) => a * b ,
+    mod : (a , b) => a % b ,
+    pow : (a , b) => a ** b ,
+    div : (a , b) =>{
+        if (b == 0){
+            return "Cant divide by zero"
+        }
+        else {
+            return a / b
+        }
+     }
+}
+
+//define numbers
+const num = ['1','2','3','4','5','6','7','8','9','0','.'];
+const oper = ['+','-','*','/','^','%'];
+const eq = '='
+// get elements
+var button = document.querySelectorAll('button')   //all buttons
+var clear = document.getElementById("clear")
+var input = document.getElementById("input")
+var cut = document.getElementById("cancel")
+var equal = document.getElementById('equate')
+var inputKey = document.querySelectorAll('input')
+
+//styling elements
+document.getElementById('clear').style.color = "#739394"
+
+
+//decalaration
+var flag
+var operand1; 
+var operand2;
+var  operation; 
+var temp ='' ;
+var operatorTemp;
+
+
+
+// functions defination  
+
+const clearHistory = () =>{
+    operand1 = undefined;
+    operatorTemp = undefined
+    operand2 = undefined ;
+    operation = undefined;
+    temp = ""
+}
+
+
+const doCalc = (op1 , op2 , calc) => {
+     res = calc(op1 , op2)
+console.log(op1,op2,calc,typeof op1 ,typeof op2)
+    if(res == "Cant divide by zero"){   //user try to div number by 0
+        alert("Cant divide by zero")
+        clearHistory() 
+        //input.value=res       //show error
+        input.value =""   
+        flag = -1
+       
+    }
+    else{                  //all goes fine
+    operand1 = res ;      //set 1st operand to computed value
+    operation = operatorTemp
+    if (operatorTemp)    //if another operator after result computed 
+    {
+        res = res + input.value.slice(-1)   //show in inputBar
+    }
+
+    input.value = res      //show computed value to input bar
+
+    operatorTemp = undefined   //reset operatorTemp
+    operand2 = undefined;     //reset 2nd operand
+    temp = ""                 //reset temp , to re-get value for 2nd operand
+    }
+}
+
+const buttonPressed = function(e)
+{
+    let btn = e.target
+    // set 2nd operand value
+    if (btn.value in operator && operand1 !== undefined){
+        if(temp == ''){     //if user overwrite operator 
+            operation = btn.value
+            input.value = input.value.slice(0,-1)
+        }
+        else{
+        operand2 = temp;
+        operatorTemp = btn.value
+        }
+    }
+    // set 1st operand value
+    else if ((btn.value in operator) && (operand2 === undefined)){
+        operand1 = input.value;
+        console.log(operand1)
+        operation = btn.value
+
+    }
+    // show value to input bar except AC , x (cross) , =
+    if (btn.innerHTML !== clear.innerHTML && btn.innerHTML !== cut.innerHTML && btn.innerHTML !== equal.innerHTML)
+     {
+        if(flag !== undefined)  // if history was clear
+         {
+            if(btn.value in operator)   //if  user press operator once history clear
+            {
+                if(flag == -1)      //all clear
+                {
+                    input.value = '0'
+                    console.log('yes')
+                }
+                else     
+                {                     //res was store
+                    input.value = res;
+                }
+            }
+           else{
+                input.value=btn.value  //initilize inputbar to  input
+           }
+            
+            flag = undefined   //reset flag
+         }
+         // get value for 2nd operand and store it in temp until user press another operator
+        if (operand1 !== undefined && !(btn.value in operator)){
+            temp+=btn.innerText
+        }
+        input.value += btn.innerText   //addd value to input
+    }  
+    // if both operand , then perfrom operation
+    if (operand1 !== undefined && operand2 !== undefined && operation !== undefined)
+    {
+        doCalc(Number(operand1),Number(operand2),operator[operation])
+
+    }
+}
+
+// add events to elements
+
+
+//adding functionality to AC button
+clear.addEventListener("click", () =>{ 
+    input.value = "";
+    clearHistory();
+    flag = -1
+})
+//adding funcitonality to cross button
+cut.addEventListener("click" ,() => {
+    input.value = input.value.slice(0,-1)
+    if(temp.length != 0){    //if user want to modified 2nd operand
+        console.log('op2')
+        temp = temp.slice(0,-1)
+        console.log(temp)
+    }
+    else if(operation !== undefined){   //if user want to modified operator
+        console.log('op')
+        operation = undefined;
+        operand1 = undefined;
+    }
+})
+
+//adding functionality to equal-to
+equal.addEventListener("click",() => {
+    operand2 = temp;
+    doCalc(Number(operand1),Number(operand2),operator[operation])
+    clearHistory();
+    if(!flag) flag = 1             //if no error
+
+})
+
+//add click event to button
+button.forEach(btn => btn.addEventListener("click",buttonPressed ))
+
+
+//any keyPressed
+inputKey.forEach(inp => inp.addEventListener("keyup", (event) => {
+    // if (!(event.key in num || event.key in oper || event.keyCode == 13 || event.keyCode == 8 || event.keyCode == 61)){
+    //     console.log('undefined operand')
+    //     alert('undefined operation')
+    //     input.value = input.value = ""
+    //     clearHistory()
+    //     flag == -1
+    // }
+    if(event.value in num || event.value in oper || event.value === '=' || event.keyCode==13 || event.keyCode == 8 ){
+        console.log('undefined operand')
+        alert('undefined operation')
+         input.value = input.value = ""
+        clearHistory()
+        flag == -1
+    }
+}))
+
