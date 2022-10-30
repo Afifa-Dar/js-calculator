50b3ad0bb8f171fb9a8b42b798d3fdd40ba73e34
Afifa Dar <afifadar2001@gmail.com>
2022-10-30 13:17:46 +0500

    upload image

--- imageValidation/index.js
+++ imageValidation/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const multer = require('multer')
+//const upload = multer({ dest: 'uploads/' })
+
 const app = express()
 
  app.use(express.static('public'))
 
-app.post('/img', (req ,res ) => {
+app.post('/img',multer().single('img')  , (req ,res ,next) => {
     console.log("post")
-    if(!req.files) {
-        console.log("req" , req.fils)
+    if(!req.file) {
+        console.log("req" )
         return res.status(400).send("No file found")
     }
-    console.log(req.files)
+    console.log(req.file)
+    if(req.file.mimetype == 'image/png') 
+        console.log('img upload')
     res.end()
 })
+
 
 app.listen(3000 , () => console.log("listen"))
